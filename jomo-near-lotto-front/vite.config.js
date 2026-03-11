36206fe93cc6df32512db7bd6e5f45998202322d
Serhii Kyba <serhiikyba@Serhiis-MacBook-Pro.local>
2026-03-11 15:34:20 +0200

    fix for mobile wallet

--- jomo-near-lotto-front/vite.config.js
+++ jomo-near-lotto-front/vite.config.js
@@ -11,4 +11,21 @@ export default defineConfig({
       buffer: 'buffer',
     },
   },
+
+  server: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+    },
+  },
+  preview: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+  },
+
+  optimizeDeps: {
+    include: ['buffer'],
+  }
 })
